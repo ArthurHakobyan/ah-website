bca45ae054e68720c1daac0810208e16140bba9f
Artur Hakobyan <h4arthakobyan@gmail.com>
2023-09-26 23:45:54 +0200

    fix the my works background image too

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        'dark-blue': '#222831',
-      'gray': '#393E46',
-      'azure': '#00ADB5',
-      'cream': '#EEEEEE',
+        "dark-blue": "#222831",
+        gray: "#393E46",
+        azure: "#00ADB5",
+        cream: "#EEEEEE",
       },
 
       backgroundImage: {
-        'works-bg': "url('/images/graphics/my-recent-works/doodle items.png')",
-      }
+        "works-bg": "url('/static/graphics/my-recent-works/doodle items.png')",
+      },
     },
   },
   plugins: [],
-}
+};
